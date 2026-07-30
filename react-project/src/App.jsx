4f293a186377ab4c09abc7734a360509c244c8be
RajkumarRj <rajpersonale@gmail.com>
2026-07-30 10:49:16 +0530

    added user input

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -6,6 +6,7 @@
 
 import { useState } from "react";
 import Child from "./Child";
+import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -27,7 +28,12 @@ function App() {
     },
   ]);
 
-  console.log(tasks);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [date, setDate] = useState("");
+
+  console.log(title);
 
   // array of object
 
@@ -37,40 +43,119 @@ function App() {
   // }
 
   // console.log(obj.name , obj.course );
+  const addTask = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      priority,
+      dueDate : date,
+      status: "Pending",
+    };
+
+    console.log(newTask);
+
+    setTasks([...tasks, newTask]);
+
+    setTitle("");
+    setDescription("");
+    setPriority("");
+    setDate("");
+  };
 
   return (
-    <div>
+    <div className="parent-container">
+      
+      <section className="form-container">
+        <form onSubmit={(e) => addTask(e)}>
+          {/* get input from user for task title  */}
+          <div className="form-group">
+            <label htmlFor="">Task title</label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Enter Task title"
+            />
+          </div>
+          {/* description  */}
+          <div className="form-group">
+            <label htmlFor="">Description</label>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Enter Description for your title"
+            />
+          </div>
+
+          {/* priority  */}
+
+          <div className="form-group">
+            <label htmlFor="">Priority</label>
+            <select
+              value={priority}
+              onChange={(e) => setPriority(e.target.value)}
+            >
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+
+              <option value="High">High</option>
+            </select>
+          </div>
+
+          {/* Date  */}
+          <div className="form-group">
+            <label htmlFor=""> Due Date</label>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              placeholder="Enter Description for your title"
+            />
+          </div>
+
+          {/* submit */}
+
+          <div className="form-actions">
+            <button type="submit">Add Task</button>
+          </div>
+        </form>
+      </section>
       {/* list rendering  */}
 
       {/* conditional rendering  */}
 
-      <table border={5}>
-        <tr>
-          <th>S.no</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>priority</th>
-          <th>Due date</th>
-          <th>Status</th>
-        </tr>
-
-        {tasks.length === 0 ? (
+      <section className="table-container">
+        <table border={5}>
           <tr>
-            <td colSpan={6}>Task not found</td>
+            <th>S.no</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>priority</th>
+            <th>Due date</th>
+            <th>Status</th>
           </tr>
-        ) : (
-          tasks.map((task) => (
+
+          {tasks.length === 0 ? (
             <tr>
-              <td>{task.id}</td>
-              <td>{task.title}</td>
-              <td>{task.description}</td>
-              <td>{task.priority}</td>
-              <td>{task.dueDate}</td>
-              <td>{task.status}</td>
+              <td colSpan={6}>Task not found</td>
             </tr>
-          ))
-        )}
-      </table>
+          ) : (
+            tasks.map((task) => (
+              <tr>
+                <td>{task.id}</td>
+                <td>{task.title}</td>
+                <td>{task.description}</td>
+                <td>{task.priority}</td>
+                <td>{task.dueDate}</td>
+                <td>{task.status}</td>
+              </tr>
+            ))
+          )}
+        </table>
+      </section>
 
       {/* 
       JSX 
