460a0fafb7a6d2a809c28b81631c2a0181c52fb3
RajkumarRj <rajpersonale@gmail.com>
2026-08-05 10:49:25 +0530

    added mongodb

---
+++ backend/model/TaskSchema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low",
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Completed"],
+    default: "Pending",
+  },
+});
+
+const Task = mongoose.model("tasks", taskSchema);
+
+export default Task;
---
+++ backend/routes/demoRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Task from "../model/TaskSchema.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Api is working");
+});
+
+router.post("/add", (req, res) => {
+  const task = new Task(req.body);
+  task.save();
+  res.json(task);
+});
+export default router;
